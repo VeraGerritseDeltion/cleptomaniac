b6c69018cd8ca234f508099611b16a2dd8b61495
GMD1AVeraGerritse <arev_97@live.nl>
2017-05-29 13:00:52 +0200

    movement

--- UnityProject/Cleptomaniac/Assets/Scripts/Vera/Movement.cs
+++ UnityProject/Cleptomaniac/Assets/Scripts/Vera/Movement.cs
@@ -8,12 +8,21 @@ public class Movement : MonoBehaviour
     public float mouseSen;
     public float horizontal;
     public float vertical;
+    public float vert;
     public float movVert;
     public float movHor;
+    public float upDownRange;
+    public Camera cam;
+    public Rigidbody player;
+    public float jumpHight;
+    public bool jumped;
 
     void Update()
     {
         MovementChar();
+        if (jumped == false){
+            Jump();
+        }
     }
 
     public void MovementChar()
@@ -22,9 +31,26 @@ public class Movement : MonoBehaviour
         vertical = Input.GetAxis("Mouse Y") * mouseSen * Time.deltaTime;
         movVert = Input.GetAxis("Vertical") * movSpeed * Time.deltaTime;
         movHor = Input.GetAxis("Horizontal") * movSpeed * Time.deltaTime;
-
-        transform.Translate(new Vector3(movVert, 0, movHor) );
-
+        transform.Translate(new Vector3(movHor, 0, movVert) );
         transform.Rotate(0, horizontal, 0);
+        vert -= vertical;
+        vert = Mathf.Clamp(vert, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(vert, 0, 0);
+    }
+
+    public void Jump()
+    { 
+        if (Input.GetButtonDown("Jump")){
+            player.AddForce(transform.up * jumpHight);
+            jumped = true;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Floor")
+        {
+            jumped = false;
+        }
     }
 }
