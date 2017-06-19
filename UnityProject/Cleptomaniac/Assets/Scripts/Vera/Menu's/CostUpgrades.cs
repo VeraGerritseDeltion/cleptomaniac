4a02aefb3d1052c2749c6b4627bb2a7b64f6ef54
GMD1AVeraGerritse <arev_97@live.nl>
2017-06-19 11:21:04 +0200

    mijs building

---
+++ UnityProject/Cleptomaniac/Assets/Scripts/Vera/Menu's/CostUpgrades.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostUpgrades : MonoBehaviour {
+    public UpgradeMenu upgrade;
+    public int id;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    public void GiveId()
+    {
+        upgrade.costOnMouse.text = upgrade.ShowCost(id).ToString();
+        upgrade.costOnMouse.enabled = true;
+    }
+    public void ExitMouse()
+    {
+        upgrade.costOnMouse.enabled = false;
+        upgrade.upgrading = false;
+    }
+}
