23ecd2a9fe56236ab15acd61c7bb02bb6466cea3
HanHakkenberg <han.hakkenberg@gmail.com>
2017-06-22 15:51:09 +0200

    Camera added

---
+++ UnityProject/Cleptomaniac/Assets/Models/Han/CameraAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAI : MonoBehaviour{
+    public Transform player;
+    public int angleOfSight;
+    public Transform lookDirection;
+    public RaycastHit rayHit;
+    public AIManager aim;
+    public int turningSpeed;
+    public int maxAngle;
+    public bool turningSide;
+    public float turn;
+    public bool willem;
+
+    void Update(){
+        if(aim.looking == true){
+            StealNoticeVoid();
+        }
+        if(turningSide == true){
+            turn += turningSpeed * Time.deltaTime;
+        }
+        else{
+            turn -= turningSpeed * Time.deltaTime;
+        }
+        turn = Mathf.Clamp(turn,-maxAngle,maxAngle);
+        transform.localEulerAngles = new Vector3(0,TurningSide(turn),transform.rotation.z);
+    }
+
+    public void StealNoticeVoid(){
+        print("ja");
+        Vector3 forward = transform.right;
+        Vector3 toPlayer = (player.position - transform.position).normalized;
+        if(Vector3.Dot(forward,toPlayer) > 0){
+            print("1");
+            float angle = Vector3.Angle(toPlayer,forward);
+            if(angle < angleOfSight){
+                print("2");
+                Debug.DrawRay(transform.position,transform.forward * 8);
+                lookDirection.LookAt(player);
+                if(Physics.Raycast(lookDirection.position,lookDirection.forward,out rayHit,8) && rayHit.collider.tag == "Player"){
+                    print("3");
+                    AIManager.wantedState = true;
+                    Debug.DrawRay(lookDirection.position,lookDirection.forward * 8);
+                }
+            }
+        }
+    }
+
+    float TurningSide(float sideToTurn){
+        if(sideToTurn == maxAngle){
+            turningSide = false;
+        }
+        else if(sideToTurn == -maxAngle) {
+            turningSide = true;
+        }
+        return (sideToTurn);
+    }
+}
